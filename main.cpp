5d1640938503ba04ef4aa8e0f7b58ccbefe439a1
JohanTv <40219652+JohanTv@users.noreply.github.com>
2019-08-22 19:25:06 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
-
 using namespace std;
 
-int main(int argc, char *argv[]) {
-    return 0;
+int main() {
+	string linea;
+	bool comillas=true;
+	while(getline(cin,linea)){
+		for(int i=0; i < linea.size(); i++){
+			if(linea[i]=='"' && comillas){
+				cout<<"``";
+				comillas=false;
+			}
+			else if(linea[i]=='"' && !comillas){
+				cout<<"''";
+				comillas=true;
+			}
+			else{
+				cout<<linea[i];
+			}
+		}
+		cout<<endl;
+	}
+	return 0;
 }
